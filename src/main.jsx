0d009fa14c28817233396d0176ba55494383396a
hadi-salloum-hue <hhood5916@gmail.com>
2026-03-13 02:28:19 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 
-
-const routes= createBrowserRouter([{
-
-  }]
-  ,{
-    basename:"/trippy"
-  })
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter basename="/Trippy">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
